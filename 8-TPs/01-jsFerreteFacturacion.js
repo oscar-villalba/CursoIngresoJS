6b82f5d8f51191c1ecc41ee3fccdbf4e1fd27542
oscar-villalba <villalbapaezoscar@gmail.com>
2021-07-07 23:23:15 -0300

    Prueba de TPs 1 y 2

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,70 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	var precioUno;
+    var precioDos;
+    var precioTres;
+    var suma;
+
+    precioUno = document.getElementById("txtIdPrecioUno").value;
+    precioDos = document.getElementById("txtIdPrecioDos").value;
+    precioTres = document.getElementById("txtIdPrecioTres").value;
+
+    precioUno = parseFloat(precioUno);
+    precioDos = parseFloat(precioDos);
+    precioTres = parseFloat(precioTres);
+
+
+    suma = precioUno + precioDos + precioTres;
+
+    alert("La suma de estos precios es de: $" + suma +".")
+
+
 }
 function Promedio () 
 {
-	
+	var precioUno;
+    var precioDos;
+    var precioTres;
+    var promedio;
+
+    precioUno = document.getElementById("txtIdPrecioUno").value;
+    precioDos = document.getElementById("txtIdPrecioDos").value;
+    precioTres = document.getElementById("txtIdPrecioTres").value;
+
+    precioUno = parseFloat(precioUno);
+    precioDos = parseFloat(precioDos);
+    precioTres = parseFloat(precioTres);
+
+
+    promedio = (precioUno + precioDos + precioTres) / 3;
+
+    alert("El promedio de estos precios es de: $" + promedio +".")
+
 }
 function PrecioFinal () 
 {
-	
+	var precioUno;
+    var precioDos;
+    var precioTres;
+    var suma;
+    var impuestos;
+
+    precioUno = document.getElementById("txtIdPrecioUno").value;
+    precioDos = document.getElementById("txtIdPrecioDos").value;
+    precioTres = document.getElementById("txtIdPrecioTres").value;
+
+    precioUno = parseFloat(precioUno);
+    precioDos = parseFloat(precioDos);
+    precioTres = parseFloat(precioTres);
+
+
+    suma = precioUno + precioDos + precioTres;
+
+    impuestos = suma * 1.21
+
+
+
+    alert("La suma total más IVA de estos precios es de: $" + impuestos +".")
+
 }
--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -1,12 +1,28 @@
 /*2.	Para el departamento de Construcción:
-A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
+A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular 
+y se debe alambra con tres hilos de alambre.
 B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
 
 function Rectangulo () 
 {
+    var largo;
+    var ancho;
+    var radio;
+    var rectangulo;
 
+    largo = document.getElementById("txtIdLargo").value;
+    ancho = document.getElementById("txtIdAncho").value;
+    radio = document.getElementById("txtIdRadio").value;
+
+    largo = parseFloat(largo);
+    ancho = parseFloat(ancho);
+    radio = parseFloat(radio);
+
+    rectangulo = (largo * 2 + ancho *2 )*3;
+
+    alert("La cantidad necesaria para hacer el rectangulo de tres partes es de " +  rectangulo + " metros de alambre.");
 }
 function Circulo () 
 {
@@ -14,5 +30,35 @@ function Circulo ()
 }
 function Materiales () 
 {
-	
+	var largo;
+    var ancho;
+    var area;
+    var bolsaCemento;
+    var bolsaCal;
+    var rellenoCemento;
+    var rellenoCal;
+
+    largo = document.getElementById("txtIdLargo").value;
+    ancho = document.getElementById("txtIdAncho").value;
+
+    largo = parseFloat(largo);
+    ancho = parseFloat(ancho);
+
+    bolsaCemento = 2;
+    bolsaCal = 3;
+
+    area = (largo + ancho ) /2;
+
+    rellenoCemento = area * bolsaCemento;
+    rellenoCal = area * bolsaCal;
+
+    alert("Se necesitan " + rellenoCemento + " bolsas de cemento y " + bolsaCal + " bolsas de cal.");
+
+
+
+
+
+    
+
+
 }
